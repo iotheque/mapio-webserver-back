81101da143328562772cf47fc76e6b525111c325
Pierrick Curt <pierrickcurt@gmail.com>
2025-02-20 18:19:16 +0100

    feat: move to a generique docker view
    This new view allows to interact with all services in docker compose file.
    It also show the port in use by each container.

--- src/mapio_webserver_back/app/server.py
+++ src/mapio_webserver_back/app/server.py
@@ -6,12 +6,16 @@ import json
 import logging
 import os
 import time
+from collections import defaultdict
 from enum import Enum
 from pathlib import Path
 from typing import Any, Union
 
+import yaml
 from flask import Flask, Response, request
 from flask_cors import CORS
+
+YAML_FILE = "/home/root/mapio/docker-compose.yml"
 
 
 class UpdateStatus(str, Enum):
@@ -143,62 +147,57 @@ def create_app() -> Flask:
         logger.info(f"SSIDs : {ssids}")
         return json.dumps(ssids)
 
-    @app.route("/docker", methods=["POST", "GET"])
-    def docker() -> Union[str, Response]:
+    @app.route("/compose", methods=["POST", "GET"])
+    def compose() -> Union[str, Response]:
         """Docker setup page.
 
         Returns:
             str: The setup docker page
         """
         if request.method == "POST":
-            data = request.form.to_dict(flat=False)
-            extract_data: Any = json.loads(data.popitem()[0]) if data else None
+            data = request.form.to_dict().popitem()[0]
+            json_data: Any = json.loads(data) if data else None
+            logger.info(f"data extract is {json_data}")
+            services = json_data.get("selectedServices")
+            logger.info(f"services {services}")
+            for service in services:
+                action: Any = json_data.get("select_action")
+                if action == "pull":
+                    os.popen(f"docker compose -f {YAML_FILE} pull {service.lower()}").read()  # noqa
+                    os.popen(
+                        f"docker compose -f {YAML_FILE} up -d --force-recreate {service.lower()}"  # noqa
+                    ).read()
+                    os.popen("docker system prune -f").read()  # noqa
+                elif action == "create":
+                    os.popen(
+                        f"docker compose -f {YAML_FILE} up -d --force-recreate {service.lower()}"  # noqa
+                    ).read()
+                else:
+                    logger.error(f"Unknown action {action}")
+                logger.info(f"docker {action} {service.lower()}")
 
-            logger.info(f"data extract is {extract_data}")
-            # Get asked action
-            action = None
-            for item in iter(extract_data):
-                if item.get("action") is not None:
-                    action = item.get("action")
-                    logger.info(f"Asked action is : {action}")
-
-            # Execute the action if needed
-            for item in iter(extract_data):
-                service: Any = item.get("service")
-                if service is not None:
-                    if item.get("selected"):
-                        if action == "restart":
-                            os.popen(
-                                f"docker compose -f /home/root/mapio/docker-compose.yml restart {service.lower()}"  # noqa
-                            ).read()
-                        elif action == "stop":
-                            os.popen(
-                                f"docker compose -f /home/root/mapio/docker-compose.yml stop {service.lower()}"  # noqa
-                            ).read()
-                        elif action == "update":
-                            os.popen(
-                                f"docker compose -f /home/root/mapio/docker-compose.yml \
- pull {service.lower()} && docker compose -f /home/root/mapio/docker-compose.yml up -d --force-recreate {service.lower()}"  # noqa
-                            ).read()
-                        else:
-                            logger.error("Unknown action")
             return Response(response="docker", status=200)
 
         if request.method == "GET":
-            logger.info("getDocker")
-            output = os.popen("docker ps --format '{{.Names}}'").read()  # noqa
+            logger.debug("getDocker")
+
             containers: list[dict[str, str]] = []
-            for line in output.splitlines():
-                line = line.rstrip("\n")
-                container = {"name": line}
-                containers.append(container)
-            logger.info(f"Containers : {containers}")
+            with Path.open(Path("/home/root/mapio/docker-compose.yml"), "r") as file:
+                compose_data = yaml.safe_load(file)
+                for service, config in compose_data.get("services", {}).items():
+                    image = config.get("image", "None")
+                    name = image.split(":")[0]
+                    version = image.split(":")[-1] if ":" in image else "latest"
+                    container = {"name": service, "image": name, "version": version}
+                    containers.append(container)
+
+            logger.debug(f"Containers : {containers}")
             return json.dumps(containers)
 
         return Response(response="docker", status=404)
 
-    @app.route("/docker-custom", methods=["POST", "GET"])
-    def docker_custom() -> Union[str, Response]:
+    @app.route("/docker", methods=["POST", "GET"])
+    def docker() -> Union[str, Response]:
         """Get Docker spectific running container.
 
         Returns:
@@ -216,11 +215,25 @@ def create_app() -> Flask:
         if request.method == "GET":
             output = os.popen("docker ps -a --format '{{.Names}} {{.Status}}'").read()  # noqa
             containers: list[dict[str, str]] = []
+
+            ports: Any = defaultdict(list)
+            scan_port = os.popen("/home/root/tools/docker_scan_port.sh").read()  # noqa
+            for line in scan_port.splitlines():
+                line = line.rstrip("\n")
+                name, port = line.split()
+                ports[name].append(port)
+
             for line in output.splitlines():
                 line = line.rstrip("\n")
-                container = {"name": line.split(" ")[0], "status": line.split(" ")[1]}
+                container_name = line.split(" ")[0]
+                container = {
+                    "name": container_name,
+                    "status": line.split(" ")[1],
+                    "port": ", ".join(map(str, ports[container_name])),
+                }
                 containers.append(container)
 
+            logger.debug(f"containers {containers}")
             return json.dumps(containers)
 
         return Response(response="docker-custom", status=404)
